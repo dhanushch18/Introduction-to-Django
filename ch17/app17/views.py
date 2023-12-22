5422294c1677e1711c66d5fb8bbb3abe41c94574
Dhanush CH <86602109+dhanushch18@users.noreply.github.com>
2023-12-22 10:06:38 +0530

    Add files via upload

---
+++ ch17/app17/views.py
@@ -0,0 +1,9 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+def fun(request):
+    return HttpResponse('hello')
+
+def joy(request):
+    return HttpResponse('Dhanush')
